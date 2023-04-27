8e3880b0aa0888055590a5b3fecd8c6a689db682
Zicklag <zicklag@katharostech.com>
2023-04-27 11:48:40 -0500

    docs: move comment about syntax to `example.sic`.

--- examples/example.rs
+++ examples/example.rs
@@ -1,12 +1,3 @@
-// I'm using a different syntax here, because the parser is dumb.
-// - [=<tag> a b c d] stands for [let <tag>(a,b) = c in d]
-// - [&<tag> a b] stands for [<tag>(a,b)]
-// - [@a b] stands for [(a b)]
-// - [#a b] stands for [λa. b]
-// - [*] stands for [()]
-// - [-] stands for an erased (unused) lambda or let variable
-// - [/a b c] inlines all occurrences of [a] by the closed term [b]
-
 use symmetric_interaction_calculus::term;
 
 pub fn main() {
